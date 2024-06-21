c6104527526f64163c0cbe935f06fdd20eab52a7
Erik Wahlberger <ewahlberger@gmail.com>
2024-06-21 21:55:23 +0200

    fix: move main logic into separate package

---
+++ internal/controller/controller.go
@@ -0,0 +1,82 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Erik142/vault-op-autounseal/internal/config"
+	"github.com/Erik142/vault-op-autounseal/internal/vault"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type AutoUnsealController struct {
+	ClientSet *kubernetes.Clientset
+	Config    *config.Config
+}
+
+func New(restConfig *rest.Config) (*AutoUnsealController, error) {
+	client, err := client.New(restConfig, client.Options{})
+
+	if err != nil {
+		panic(fmt.Errorf("Could not create Kubernetes client: %v\n", err))
+	}
+
+	clientset := kubernetes.NewForConfigOrDie(restConfig)
+
+	err = config.Init(client)
+
+	if err != nil {
+		panic(fmt.Errorf("Could not create application configuration: %v\n", err))
+	}
+
+	c, err := config.Get()
+
+	if err != nil {
+		panic(fmt.Errorf("Could not retrieve application configuration: %v\n", err))
+	}
+
+	return &AutoUnsealController{Config: c, ClientSet: clientset}, nil
+}
+
+func (self *AutoUnsealController) Reconcile() error {
+	for true {
+		_, err := self.ClientSet.AppsV1().StatefulSets(self.Config.StatefulSetNamespace).Get(context.Background(), self.Config.StatefulSetName, metav1.GetOptions{})
+
+		if err != nil {
+			fmt.Printf("Waiting for Vault Statefulset '%s' to be created...\n", self.Config.StatefulSetName)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		fmt.Printf("Found Vault Statefulset '%s'!\n", self.Config.StatefulSetName)
+		break
+	}
+
+	for true {
+		vault, err := vault.New(self.ClientSet)
+
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		if err = vault.Init(); err != nil {
+			fmt.Println(err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		if err = vault.Unseal(); err != nil {
+			fmt.Println(err)
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"path/filepath"
-	"time"
 
-	"github.com/Erik142/vault-op-autounseal/internal/config"
-	"github.com/Erik142/vault-op-autounseal/internal/vault"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"github.com/Erik142/vault-op-autounseal/internal/controller"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-var Config *config.Config
 
 func main() {
 	var kubeconfig *string
@@ -38,58 +29,9 @@ func main() {
 		}
 	}
 
-	client, err := client.New(restConfig, client.Options{})
+	c, err := controller.New(restConfig)
 
-	if err != nil {
-		panic(fmt.Errorf("Could not create Kubernetes client: %v\n", err))
-	}
-
-	clientset := kubernetes.NewForConfigOrDie(restConfig)
-
-	err = config.Init(client)
-
-	if err != nil {
-		panic(fmt.Errorf("Could not create application configuration: %v\n", err))
-	}
-
-	Config, err = config.Get()
-
-	if err != nil {
+	if err := c.Reconcile(); err != nil {
 		panic(err)
-	}
-
-	for true {
-		_, err := clientset.AppsV1().StatefulSets(Config.StatefulSetNamespace).Get(context.Background(), Config.StatefulSetName, metav1.GetOptions{})
-
-		if err != nil {
-			fmt.Printf("Waiting for Vault Statefulset '%s' to be created...\n", Config.StatefulSetName)
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		fmt.Printf("Found Vault Statefulset '%s'!\n", Config.StatefulSetName)
-		break
-	}
-
-	for true {
-		vault, err := vault.New(clientset)
-
-		if err != nil {
-			fmt.Println(err)
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		if err = vault.Init(); err != nil {
-			fmt.Println(err)
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		if err = vault.Unseal(); err != nil {
-			fmt.Println(err)
-		}
-
-		time.Sleep(5 * time.Second)
 	}
 }
